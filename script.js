b36433d16e60e3c617f0cc63c8d76826adf3d2ce
David Blagojevic <d.blagojevic@cyberdanube.com>
2025-09-16 14:00:33 +0200

    4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var paragraph = document.getElementById('xss');
 // Check if the element exists
 if (paragraph) {
     // Add text to the <p> element
-    paragraph.textContent = 'hi';
+    paragraph.textContent = document.cookie;
 }
 
-console.log("hello");
+console.log(document.cookie);
